634cc722dd85aac5ebf3c4a04f11f5aecf408d0d
kimishima <kimishima@geekfeed.co.jp>
2025-08-07 18:24:59 +0900

    add comment

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,7 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-
+// This file is part of the Laravel framework.
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
